ab6bbb354277fa096fe96505067988b7d0c82168
TirthCodez <159916386+TirthCodez@users.noreply.github.com>
2024-06-09 20:03:06 +0530

    Program to add 3 numbers in Java

---
+++ P2_ProgramToAddThreeNumbers.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class P2_ProgramToAddThreeNumbers {
+    public static void main(String[] args) {
+        int n1=6,n2=3,n3=8;
+        int sum=n1+n2+n3;
+        System.out.print("The sum of these numbers is: "); //print is used to print the output in same horizontal line
+        System.out.println(sum); //println adds new line after output
+    }
+}
