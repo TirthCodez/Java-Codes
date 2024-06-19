a64f9c97c02c53148cbe045fa2a14798fde3751d
TirthCodez <159916386+TirthCodez@users.noreply.github.com>
2024-06-19 20:37:08 +0530

    Literals in Java

---
+++ P3_LiteralsInJava.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class P3_LiteralsInJava {
+    public static void main(String []args){
+        //The value stored in the variable are called literals i.e. on the right side of "=" sign
+        int age=54;
+        byte age1=24;
+        short age3=67;
+        long age4= 5754754754747L; //for long type of variable writing "L" in end is important otherwise it is interpreted as integer
+        float marks=89.99f; //for float writing "f" or "F" in end is must because default decimal datatype is double
+        double marks1=66.66;
+        String str="Java";
+        boolean a = true;
+    }
+}
